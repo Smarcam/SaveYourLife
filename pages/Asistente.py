f8f88e45caf7308daa207426690a9a4bd1b312cd
Smarcam <121159130+Smarcam@users.noreply.github.com>
2023-02-22 19:57:43 +0100

    Update Asistente.py

--- pages/Asistente.py
+++ pages/Asistente.py
@@ -1,63 +1,42 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
-import streamlit.components.v1 as components
-from PIL import Image
+import streamlit as st 
 from selenium import webdriver
+from nltk.chat.util import Chat, reflections
 
-icon = Image.open('img/icon.png')
-st.set_page_config(
-	page_title = 'SaveYourLife Tumor Brain Predict!',
-	page_icon = icon,
-	layout = 'wide',
-	initial_sidebar_state = 'collapsed',
-	menu_items={
-		'Get Help': 'https://streamlit.io',
-		'Report a bug': 'https://github.com',
-		'About':'About your application: **Hello World!**'
-	}
-	)
-st.sidebar.title("Main Menu")
+def google_search(term):
+    if term:
+        global browser
+        browser = webdriver.Chrome()
+        browser.get(f"https://www.google.com/search?q={term}")
 
-def chatbot(user_input):
-    response = ""
-    if "hola" in user_input:
-        response = "Hola! ¿Cómo puedo ayudarte?"
-    elif "asistente" in user_input or "llamas" in user_input:
-        response = "Me llamo Josua."
-    elif "hacer" in user_input:
-        response = "Estoy aquí para ayudarte con cualquier pregunta que tengas."
-    elif "información" in user_input:
-        query = user_input.replace("información", "").strip()
-        results = search_google(query)
-        if not results:
-            response = "No he encontrado resultados."
-        else:
-            response = "Aquí tienes lo que he encontrado en Google:"
-            for result in results:
-                response += "\n- [{}]({})".format(result["title"], result["link"])
+        # Espera hasta que se cargue la página de resultados
+        browser.implicitly_wait(5)
+
     else:
-        response = "Lo siento, parece que hay un problema con tu mensaje. Por favor, escríbelo de nuevo."
-    return response
+        st.warning("Por favor, ingrese un término de búsqueda.")
 
-def search_google(query):
-    results = []
-    driver = webdriver.Firefox()
-    driver.get(f"https://www.google.com/search?q={query}")
-    links = driver.find_elements_by_css_selector("div.r a")
-    for link in links:
-        title = link.find_element_by_css_selector("h3").text
-        url = link.get_attribute("href")
-        results.append({"title": title, "link": url})
-    driver.quit()
-    return results
+pairs = [
+    ['mi nombre es (.*)', ['Hola! %1. Soy la Yeny. En que puedo ayudarte?']],
+    ['hola', ['Hola Soy La Yeny. Que cojones quieres?????']],
+    ['que puedes hacer', ['puedo hacer muchas cosas o que te crees subnormal, me tienes mas agobia que spiderman en un descampado.']],
+    ['abrir (.*)', ['Lo siento, yo puedo buscarte %1 . Estoy en el parque se solipandi. y no puedo hacer esas cosas chikillo que me va a ve mi mare.']],
+    ['piensas si hay un creador', ['si, aro ompare si no como voy a esta aqui hablando']],
+    ['dime algo sobre ti', ['Pos aye estuve de aguaora por que venia la tres patos con mi primo']],
+    ['quien soy', ['a mi que coño me importa"']],
+    ['quien te creo', ['un jartible.']],
+]
 
-if __name__ == '__main__':
-    st.title("Chatbot")
-    user_input = st.text_input("Introduce tu mensaje:", value="soy tu asistente, en que puedo ayudarte").lower()
-    if st.button('Enviar'):
-        response = chatbot(user_input)
-        st.write(response)
- # LINK TO THE CSS FILE
-with open('.streamlit/style.css')as f:
- st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+st.title("Chatbot")
+
+
+def main():
+    ref = st.text_input("Dime argo cojones")
+
+    chat = Chat(pairs, reflections)
+    respo = chat.respond(ref)
+    if "abrir" in ref:
+        search_term = ref.split("abrir")[1]
+        google_search(search_term)
+    st.write(respo)
+
+if __name__ == "__main__":
+    main()
