7ec73cea308909198ad2e95331cc4d8b3c94f145
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-01 19:18:01 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -19,7 +19,7 @@ with st.sidebar:
             selected = option_menu(
                 menu_title="Main Menu",  # required
                 options=["Inicio", "Brain tumor predict ", "Asistente", "About me"],  # required
-                icons=["house", "bi bi-clipboard2-pulse", "envelope", "envelope"],  # optional
+                icons=["file-earmark-medical", "bi bi-clipboard2-pulse", "envelope", "envelope"],  # optional
                 menu_icon="bi bi-hospital",  # optional
                 default_index=0,  # optional
             )
