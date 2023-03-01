b2c81d2f5484b4abbcd902d7d857ca853b808b31
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-01 18:00:16 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import streamlit as st
 # LINK TO THE CSS FILE
 def importar_css():
   with open('.streamlit/style.css')as f:
      st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+# selenium
+chrome_options = Options()
+chrome_options.add_argument("--headless") # Para ejecutar en modo headless
+driver = webdriver.Chrome(options=chrome_options)
+def navigate_to_search(term):
+    driver.get(f"https://www.google.com/search?q={term}")
