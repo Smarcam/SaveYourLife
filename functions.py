85ce678f235b6db66cf174694e46a7518850c43b
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-01 17:05:44 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
 # LINK TO THE CSS FILE
-def importar_css(style):
+def importar_css():
   with open('.streamlit/style.css')as f:
      st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
