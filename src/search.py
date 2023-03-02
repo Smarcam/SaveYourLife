81aae523c618fe5fd3cdb66899be6b9e94e2f3ed
Smarcam <121159130+Smarcam@users.noreply.github.com>
2023-03-02 17:09:55 +0100

    Update search.py

--- src/search.py
+++ src/search.py
@@ -1,27 +1,16 @@
 import streamlit as st
-import webbrowser
 from googlesearch import search as google_search
 
 
-def my_google_search(query):
-    if query:
-        results = []
-        for j in google_search(query):
-            results.append(j)
-        return results
-    else:
-        st.warning("Por favor, ingrese un término de búsqueda.")
-
-
-def on_enter_pressed(term):
+def on_enter_pressed(term, num_results=10):
     if term:
-        results = my_google_search(term)
-        if results:
-            # Abre la búsqueda en una nueva pestaña del navegador
-            url = f"https://www.google.com/search?q={'+'.join(term.split())}"
-            st.markdown(f"[Resultados de la búsqueda]({url})")
-            st.stop()
-
-        else:
-            st.warning("No se encontraron resultados para su búsqueda.")
+        try:
+            results = list(google_search(term, num_results=num_results))
+            if results:
+                url = f"https://www.google.com/search?q={'+'.join(term.split())}"
+                st.markdown(f"[Resultados de la búsqueda]({url})")
+            else:
+                st.warning("No se encontraron resultados para su búsqueda.")
+        except StopIteration:
+            st.warning("No se encontraron resultados para su búsqueda o se alcanzó el límite máximo de solicitudes.")
 
