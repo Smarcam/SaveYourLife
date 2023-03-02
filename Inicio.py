d3cdfcea0490041957b7dc527e707db7f713b582
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-02 16:56:37 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from streamlit_option_menu import option_menu
 from PIL import Image
 import src.search as srch
 import src.load_event as le
