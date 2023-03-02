d7c88abd5dc01c9c34c4e5c41c6ef32a70c9f076
Smarcam <121159130+Smarcam@users.noreply.github.com>
2023-03-02 13:51:30 +0100

    Update search.py

--- src/search.py
+++ src/search.py
@@ -20,6 +20,8 @@ def on_enter_pressed(term):
             # Abre la búsqueda en una nueva pestaña del navegador
             url = f"https://www.google.com/search?q={'+'.join(term.split())}"
             webbrowser.open_new_tab(url)
+            st.stop()
+
         else:
             st.warning("No se encontraron resultados para su búsqueda.")
 
