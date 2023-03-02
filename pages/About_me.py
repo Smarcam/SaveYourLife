d572f797306a6da6a0c105f186d1ce9e1878fc29
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-02 16:32:58 +0100

    Update About_me.py

--- pages/About_me.py
+++ pages/About_me.py
@@ -4,19 +4,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 from functions import *
 
-st.set_page_config(
-	page_title = 'SaveYourLife Tumor Brain Predict!',
-	page_icon='img/icon.png',
-	layout = 'wide',
-	initial_sidebar_state = 'collapsed',
-	menu_items={
-		'Get Help': 'https://streamlit.io',
-		'Report a bug': 'https://github.com',
-		'About':'About your application: **Hello World!**'
-	}
-	)
-st.sidebar.title("Main Menu")
-
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=["a", "b", "c"])
