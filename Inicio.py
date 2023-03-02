a4cf84a90b787ded88c47f3180d66b8c9b8cf5bd
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-02 21:24:02 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
+from streamlit_card import card
 from PIL import Image
 import src.search as srch
 import src.load_event as le
@@ -113,6 +114,10 @@ with col2:
             un nuevo enfoque de tratamiento personalizado y una mejor comprensión de la enfermedad.")
 
 with col3:
+   st.card(
+    "Título de la tarjeta",
+    "Aquí va el texto de la tarjeta.",
+    "https://via.placeholder.com/150",)
    components.html('<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',height=420,width=350)
 #CCS
 css = importar_css()
