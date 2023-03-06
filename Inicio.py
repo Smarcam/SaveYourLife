41db6b6889688c9fda964f3716559d4483aefa20
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-06 18:11:14 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -11,8 +11,7 @@ page_config = importar_config()
 show_pages_from_config()
 
 with title_container:
-    le.image_clickable('img/logo.png', "https://saveyourlife.streamlit.app")
-
+   le.image_clickable('img/logo.png', "https://saveyourlife.streamlit.app")
 
 with st.container():
     st.header("Buscador")
