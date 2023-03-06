3794accb91d1912a300cce9813d4788d7724c9d9
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-06 11:58:42 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -16,9 +16,8 @@ col1, col2 = st.columns([3, 20])
 with title_container:
    with col1:
       le.image_clickable('img/logo.png', "https://saveyourlife.streamlit.app")
-      st.markdown('<h1 style="color: #28cffe;">SaveYourLife</h1>',unsafe_allow_html=True)
    with col2:
-      st.markdown('<h1 style="color: #28cffe;">.</h1>',unsafe_allow_html=True)
+       st.markdown('<h1 style="color: #28cffe;">SaveYourLife</h1>',unsafe_allow_html=True)
 
 with st.container():
     st.header("Buscador")
