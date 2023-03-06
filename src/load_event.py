7e422112de5566e2d6ddbdcfdb8f5887effd247f
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-06 18:35:14 +0100

    Update load_event.py

--- src/load_event.py
+++ src/load_event.py
@@ -18,8 +18,3 @@ def logo_clickable(image_path, target_url):
     clickable_image = f'<a href="{target_url}" target="_self"><img src="data:image/png;base64,{b64encode(image).decode()}" style="max-inline-size: 100%; block-size: auto; aspect-ratio: 2/1; object-fit: cover;"></a>'
     # Muestra la imagen clickeable
     st.markdown(clickable_image, unsafe_allow_html=True)
-
-  max-inline-size: 100%;
-  block-size: auto;
-  aspect-ratio: 2/1;
-  object-fit: cover;
