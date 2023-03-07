dd061dd5da9ee8b16f1b4cce70344bf0f42e686a
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-08 00:45:50 +0100

    Update Brain_tumor_predict.py

--- pages/Brain_tumor_predict.py
+++ pages/Brain_tumor_predict.py
@@ -47,19 +47,9 @@ def process_image(image):
 uploaded_file = st.file_uploader("Cargar imagen", type=["jpg", "jpeg", "png"])
 if st.button("📷"):
     img_file_buffer = st.camera_input("Take a picture")
-
     if img_file_buffer is not None:
-        # To read image file buffer as a 3D uint8 tensor with TensorFlow:
-        bytes_data = img_file_buffer.getvalue()
-        img_tensor = tf.io.decode_image(bytes_data, channels=3)
-
-        # Check the type of img_tensor:
-        # Should output: <class 'tensorflow.python.framework.ops.EagerTensor'>
-        st.write(type(img_tensor))
-
-        # Check the shape of img_tensor:
-        # Should output shape: (height, width, channels)
-        st.write(img_tensor.shape)
+        input_img = Image.open(img_file_buffer)
+        final_sketch = process_image(np.array(input_img))
 if not uploaded_file:
     st.warning("Por favor, carga una imagen para continuar.")
 else:
