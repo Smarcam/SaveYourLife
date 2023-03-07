8dadfc53c81b6323eb8323cdf9fc2eb1f1458803
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-07 22:13:25 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -13,7 +13,7 @@ page_config = importar_config()
 show_pages_from_config()
 # specify the primary menu definition
 menu_data = [
-    {'icon':"e650",'label':" "},
+    {'icon':"🩻",'label':" "},
     {'icon': "fa-solid fa-radar",'label':"Modelos", 'submenu':[{'label':"Brain", 'icon': "fa fa-meh"},{'label':"Breast"}]},
     {'id':'Copy','icon':"🐙",'label':"Asistente"},
     {'icon': "far fa-copy", 'label':"Sobre Nosotros"},
