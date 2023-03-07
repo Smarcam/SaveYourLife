48e3327ff16fd26c71dbca051282145472f13365
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-07 14:07:03 +0100

    Update Brain_tumor_predict.py

--- pages/Brain_tumor_predict.py
+++ pages/Brain_tumor_predict.py
@@ -45,22 +45,6 @@ def process_image(image):
 
 # Carga la imagen con Streamlit y muestra la predicción
 uploaded_file = st.file_uploader("Cargar imagen", type=["jpg", "jpeg", "png"])
-if st.button("📷"):
-   img_file_buffer = st.camera_input("Take a picture")
-   if img_file_buffer is not None:
-       # To read image file buffer as a PIL Image:
-       img = Image.open(img_file_buffer)
-
-       # To convert PIL Image to numpy array:
-       img_array = np.array(img)
-
-       # Check the type of img_array:
-       # Should output: <class 'numpy.ndarray'>
-       st.write(type(img_array))
-
-       # Check the shape of img_array:
-       # Should output shape: (height, width, channels)
-       st.write(img_array.shape)
 if not uploaded_file:
     st.warning("Por favor, carga una imagen para continuar.")
 else:
