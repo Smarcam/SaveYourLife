799917e8f5150c53a27962a6efc57b8074c03e7c
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-08 00:37:50 +0100

    Update Brain_tumor_predict.py

--- pages/Brain_tumor_predict.py
+++ pages/Brain_tumor_predict.py
@@ -46,21 +46,20 @@ def process_image(image):
 # Carga la imagen con Streamlit y muestra la predicción
 uploaded_file = st.file_uploader("Cargar imagen", type=["jpg", "jpeg", "png"])
 if st.button("📷"):
-   img_file_buffer = st.camera_input("Take a picture")
-   if img_file_buffer is not None:
-       # To read image file buffer as a PIL Image:
-       img = Image.open(img_file_buffer)
+    img_file_buffer = st.camera_input("Take a picture")
 
-       # To convert PIL Image to numpy array:
-       img_array = np.array(img)
+    if img_file_buffer is not None:
+        # To read image file buffer as a 3D uint8 tensor with TensorFlow:
+        bytes_data = img_file_buffer.getvalue()
+        img_tensor = tf.io.decode_image(bytes_data, channels=3)
 
-       # Check the type of img_array:
-       # Should output: <class 'numpy.ndarray'>
-       st.write(type(img_array))
+        # Check the type of img_tensor:
+        # Should output: <class 'tensorflow.python.framework.ops.EagerTensor'>
+        st.write(type(img_tensor))
 
-       # Check the shape of img_array:
-       # Should output shape: (height, width, channels)
-       st.write(img_array.shape)
+        # Check the shape of img_tensor:
+        # Should output shape: (height, width, channels)
+        st.write(img_tensor.shape)
 if not uploaded_file:
     st.warning("Por favor, carga una imagen para continuar.")
 else:
