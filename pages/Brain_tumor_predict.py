a29f48da0d20c9263bb75b2f60d8e24b401edc78
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-07 20:20:31 +0100

    Update Brain_tumor_predict.py

--- pages/Brain_tumor_predict.py
+++ pages/Brain_tumor_predict.py
@@ -3,19 +3,27 @@ import cv2
 import numpy as np
 import time
 import imutils
-import tensorflow as tf
-import keras
-from keras.optimizers import Adam
 from functions import *
+
+import keras
+from tensorflow import keras
+from tensorflow.python.keras.models import *
 # from tensorflow.keras import preprocessing
 from keras import preprocessing
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout,Embedding,LSTM,GRU
+from keras.preprocessing.image import ImageDataGenerator
 # from keras.layers.embeddings import Embedding
+from sklearn.metrics import classification_report,confusion_matrix
+import tensorflow as tf
+from keras.optimizers import Adam
+import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+
 
 #PageConfig
 page_config = importar_config()
