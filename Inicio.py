d575d9a47b7af368201f174dcc205d25fefb3280
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-07 09:44:30 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -148,19 +148,19 @@ with col3:
    
    if st.button("camrera"):
        img_file_buffer = st.camera_input("Take a picture")
-           if img_file_buffer is not None:
-               # To read image file buffer as a PIL Image:
-               img = Image.open(img_file_buffer)
+       if img_file_buffer is not None:
+           # To read image file buffer as a PIL Image:
+           img = Image.open(img_file_buffer)
 
-               # To convert PIL Image to numpy array:
-               img_array = np.array(img)
+           # To convert PIL Image to numpy array:
+           img_array = np.array(img)
 
-               # Check the type of img_array:
-               # Should output: <class 'numpy.ndarray'>
-               st.write(type(img_array))
+           # Check the type of img_array:
+           # Should output: <class 'numpy.ndarray'>
+           st.write(type(img_array))
 
-               # Check the shape of img_array:
-               # Should output shape: (height, width, channels)
-               st.write(img_array.shape)
+           # Check the shape of img_array:
+           # Should output shape: (height, width, channels)
+           st.write(img_array.shape)
 #CCS
 css = importar_css()
