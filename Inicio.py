f20e82a76c052275cf9b6d831c300d0e57a12acc
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-07 21:43:30 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -21,7 +21,6 @@ over_theme = {'txc_inactive': '#FFFFFF'}
 menu_id = hc.nav_bar(
     menu_definition=menu_data,
     override_theme=over_theme,
-    home_name=st.image('img/logo.png'),
     hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
     sticky_nav=True, #at the top or not
     sticky_mode='sticky', #jumpy or not-jumpy, but sticky or pinned
