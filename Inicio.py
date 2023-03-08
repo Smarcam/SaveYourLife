b1a813be9d70c2e8e933e565a8c4a7049d1fa329
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-08 13:04:09 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -13,6 +13,7 @@ page_config = importar_config()
 show_pages_from_config()
 
 def menu():
+  return """"
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar scroll</a>
@@ -49,6 +50,7 @@ def menu():
     </div>
   </div>
 </nav>
+"""
 st.markdown("""
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 """, unsafe_allow_html=True)
