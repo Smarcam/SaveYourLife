8061d06d1e8cb83b3c6ac2750d888b4b93bbcf6a
Smarcam <121159130+Smarcam@users.noreply.github.com>
2023-03-08 10:02:06 +0100

    Update Brain_tumor_predict.py

--- pages/Brain_tumor_predict.py
+++ pages/Brain_tumor_predict.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import time
 import imutils
-import tensorflow as tf
 import keras
 from functions import *
 
@@ -39,28 +38,27 @@ def process_image(image):
     # Añade una dimensión adicional para la dimensión del batch
     image = np.expand_dims(image, axis=0)
     # Realiza la predicción
-    discriminator = discriminator_model.predict(image)
+    prediction = discriminador_model.predict(image)
     # Devuelve la clase con la mayor probabilidad
     return np.argmax(prediction)
 
+# Función para procesar la imagen y realizar la predicción
+def process_image2(image):
+    # Redimensiona la imagen a las dimensiones de entrada del modelo
+    image = cv2.resize(image, (224, 224))
+    # Convierte la imagen a escala de grises
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Normaliza los valores de los píxeles
+    image = image / 255.0
+    # Añade una dimensión adicional para la dimensión del batch
+    image = np.expand_dims(image, axis=0)
+    # Realiza la predicción
+    prediction2 = keras_model.predict(image)
+    # Devuelve la clase con la mayor probabilidad
+    return np.argmax(prediction2)
+
 # Carga la imagen con Streamlit y muestra la predicción
 uploaded_file = st.file_uploader("Cargar imagen", type=["jpg", "jpeg", "png"])
-if st.button("📷"):
-   img_file_buffer = st.camera_input("Take a picture")
-   if img_file_buffer is not None:
-       # To read image file buffer as a PIL Image:
-       img = Image.open(img_file_buffer)
-
-       # To convert PIL Image to numpy array:
-       img_array = np.array(img)
-
-       # Check the type of img_array:
-       # Should output: <class 'numpy.ndarray'>
-       st.write(type(img_array))
-
-       # Check the shape of img_array:
-       # Should output shape: (height, width, channels)
-       st.write(img_array.shape)
 if not uploaded_file:
     st.warning("Por favor, carga una imagen para continuar.")
 else:
@@ -72,46 +70,44 @@ else:
         # Carga el modelo previamente entrenado
         with st.spinner('Cargando modelo...'):
             time.sleep(1)
-            discriminator_model = keras.models.load_model('model/discriminador.h5')
             keras_model = keras.models.load_model('model/brainmodel.h5')
-
+            discriminador_model = keras.models.load_model('model/discriminador.h5')
         # Agrega un botón para realizar la predicción, solo este visible cuando se cargue la imagen 
         button_clicked = st.button("Realizar predicción")
         if button_clicked:
-            # Crea un placeholder para la imagen gif
-            placeholder = st.empty()
-            # Muestra el spinner antes de la predicción
-            with st.spinner('Realizando predicción...'):
-                # Muestra la imagen gif en el placeholder
-                gif_ref = placeholder.image(image_2)
-                 # Espera 5 segundos para simular el procesamiento
-                time.sleep(5)
-                # Elimina la imagen gif del placeholder
-                del gif_ref
-                 # Espera 5 segundos para simular el procesamiento
-                time.sleep(1)
-                # Optimiza la imagen para acelerar la predicción
-                processed_image = imutils.resize(image, width=400)
-                # Realiza la predicción y muestra el resultado en la interfaz
-                prediction = process_image(processed_image)
-                # Elimina el placeholder
-                placeholder.empty()
-                # Muestra la imagen en la interfaz
-                st.image(image, channels="BGR", caption="Imagen cargada")
                 # Realiza la predicción y muestra el resultado en la interfaz
                 prediction = process_image(image)
                 if prediction == 0:
-                    st.write("La imagen no se corresponde con una imagen MRI")
-                elif prediction == 1:  
-                    prediction = keras_model.predict(image)
-                    if prediction == 0:
+                    st.write("La imagen no corresponde con una MRI")
+                elif prediction == 1:
+                    # Crea un placeholder para la imagen gif
+                    placeholder = st.empty()
+                    # Muestra el spinner antes de la predicción
+                    with st.spinner('Realizando predicción...'):
+                        # Muestra la imagen gif en el placeholder
+                        gif_ref = placeholder.image(image_2)
+                        # Espera 5 segundos para simular el procesamiento
+                        time.sleep(1)
+                        # Elimina la imagen gif del placeholder
+                        del gif_ref
+                        # Espera 5 segundos para simular el procesamiento
+                        time.sleep(1)
+                        # Elimina el placeholder
+                        placeholder.empty()
+                        # Muestra la imagen en la interfaz
+                        st.image(image, channels="BGR", caption="Imagen cargada")
+                        # Realiza la predicción y muestra el resultado en la interfaz
+                    prediction2 = process_image2(image)
+                    if prediction2 == 0:
                         st.write("La imagen corresponde a un cerebro con tumor tipo glioma")
-                    elif prediction == 1:  
+                    elif prediction2 == 1:  
                         st.write("La imagen corresponde a un cerebro con tumor tipo menignoma")
-                    elif prediction == 2:  
+                    elif prediction2 == 2:  
                         st.write("La imagen corresponde a un cerebro sano")
-                    elif prediction == 3:  
+                    elif prediction2 == 3:  
                         st.write("La imagen corresponde a un cerebro con tumor tipo pituitario")
+
+
     except:
         st.error("Ocurrió un error al leer la imagen cargada. Por favor, asegúrate de que el archivo que estás cargando es una imagen.")
 #CCS
