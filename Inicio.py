8adb6f3c569aca900128d5b602dd0d46d0da2fc5
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-08 19:10:56 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -14,42 +14,35 @@ show_pages_from_config()
 
 def menu():
   return """
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar scroll</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Link
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Link</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="style.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet"></head>
+
+
+<body data-spy="scroll" data-target="#menu2" data-offset="160">
+    <!-- esto es la cabecera. -->
+
+       <!-- texto explicativo. --> 
+            
+    <nav  id="menu2" class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <a class="navbar-brand" href="#"><img class="mediana" src="logop.png"></a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#mimenu2" aria-controls="mimenu2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="mimenu2" style="">
+            <ul class="navbar-nav ml-auto p2">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#secccion-1" id="btn1">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#secccion-2" id="btn2" >Sobre mí</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#secccion-3" id="btn3">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 """
 st.markdown("""
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
