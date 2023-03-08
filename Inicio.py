d1b34f9d7f856a7ddcded1c03a2050d168ff5076
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-08 21:02:56 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -13,7 +13,7 @@ page_config = importar_config()
 show_pages_from_config()
 def menu():
   return """
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="https://saveyourlife.streamlit.app" target="_self">h</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
