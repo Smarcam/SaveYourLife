17dc17c1c22c45072a56a137ae2b0bbbcc90cac1
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-08 20:56:26 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -11,32 +11,6 @@ import datetime
 page_config = importar_config()
 #Menu
 show_pages_from_config()
-
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="https://saveyourlife.streamlit.app" target="_self">h</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Acerca de</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contacto</a>
-        </li>
-      </ul>
-      <form class="d-flex ms-auto">
-        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
-        <button class="btn btn-outline-light" type="submit">Buscar</button>
-      </form>
-    </div>
-  </div>
-</nav>
   return """
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
   <div class="container-fluid">
