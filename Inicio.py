7b9a2e33f2f2664b3dc04291079eef93297aa1b9
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-09 20:21:23 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -15,20 +15,20 @@ def menu():
   return """
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" "href="https://saveyourlife.streamlit.app" target="_self">Fixed navbar</a>
+    <a class="navbar-brand" "href="https://saveyourlife.streamlit.app" target="_self">SaveYourLife</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="https://saveyourlife.streamlit.app/Asistente" target="_self">💬Asistente💬</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="https://saveyourlife.streamlit.app/Brain Tumor" target="_self>🧠Brain Tumor🧠</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href="https://saveyourlife.streamlit.app/About me" target="_self>📚About me📚</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
