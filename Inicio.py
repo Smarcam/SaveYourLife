c93d45574c9d5948bf526200370d31a8004e8a7d
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-09 20:53:13 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -19,7 +19,7 @@ def menu():
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
+    <div class="navbar-collapse collapse show" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
           <a class="nav-link" href="https://saveyourlife.streamlit.app/Asistente" target="_self">Asistente</a>
