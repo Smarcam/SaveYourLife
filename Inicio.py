31a1730bf41cba01ea6cc0f1081f20742543eba9
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-09 21:03:02 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -16,9 +16,6 @@ def menu():
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" "href="https://saveyourlife.streamlit.app" target="_self">SaveYourLife</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="navbar-collapse collapse show" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
