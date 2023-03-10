0758cb01ac2102c0821e1fc570610acc5ef0c93f
Smarcam <121159130+Smarcam@users.noreply.github.com>
2023-03-10 02:26:52 +0100

    Update Asistente.py

--- pages/Asistente.py
+++ pages/Asistente.py
@@ -1,12 +1,16 @@
 import streamlit as st
 from streamlit_chat import message
 import src.chatbot_code as chb
+import src.search as srch
 import re
+from functions import *
 
-st.set_page_config(
-    page_title="Chat SaveYourLife",
-    page_icon=":robot:"
-)
+
+
+page_config = importar_config()
+st.title("Chatbot")
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
 
 st.header("Chat SaveYourLife")
 
@@ -17,7 +21,9 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
 
 def get_text():
-    input_text = st.text_input("You: ","Hola!!", key="input")
+    input_text = st.text_input("You: ","",
+                                placeholder="¿En que puedo ayudarte?",
+                                key="input")
     return input_text
 
 def chatbot_response(user_input):
@@ -31,7 +37,30 @@ if user_input:
     st.session_state.generated.append(bot_response)
     st.session_state.past.append(user_input)
 
+if 'buscador' not in st.session_state:
+    st.session_state['buscador'] = None
+
+match = re.search(r"(busc[oa]|encontrar|hallar|búscame|busca)\b", user_input, re.IGNORECASE)
+if match:
+    # Extraer la consulta de búsqueda del usuario
+    search_term = user_input[match.end():].strip()
+    srch.on_enter_pressed(search_term)
+else:
+    split_ref = re.split(r"(búscame|busca)", user_input, re.IGNORECASE)
+    if len(split_ref) > 1:
+        search_term = split_ref[-1].strip()
+        srch.on_enter_pressed(search_term)
+    else:
+        st.write("")
+
+
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+        message(st.session_state["generated"][i], avatar_style="bottts", key=str(i))
+        message(st.session_state['past'][i], avatar_style="avataaars", is_user=True, key=str(i) + '_user')
+
+st.markdown("""
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""", unsafe_allow_html=True)
