234015ec83a4bb251066b4949ab0746157acf4c0
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-11 19:59:07 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -90,7 +90,7 @@ def cards():
 </div>
 """
 #FOOTER
-def footerinicio():
+def footermain():
   return """
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 <div class="wrapper">
