d0c34c49337369f4850fa138752b8cdbbb9d72b6
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-11 15:50:10 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -23,27 +23,7 @@ with col2:
 st.image("img/logo.png")
 st.image("img/icon.png")
 st.image("img/inicio.png")
-def menu():
-  return """
-<nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background: #0089b1">
-  <div class="container-fluid" style="margin-left: 120px">
-    <a class="navbar-brand" href="https://saveyourlife.streamlit.app" target="_self"><img src="https://saveyourlife.streamlit.app:443/~/+/media/93fd3ac7283a17deb7cf31fad8ecaa703bd9e169ca706a636245461f.png" alt="Mi imagen" style="width: 50px; margin-top: 0px; margin-left: -60px;"></a>
-    <div class="navbar-collapse collapse" id="navbarCollapse" style="margin-left: -20px;">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item">
-          <a class="nav-link" href="https://saveyourlife.streamlit.app/Asistente" target="_self" style="color:#d1e9ff;font-size: 20px;">Asistente</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://saveyourlife.streamlit.app/Brain Tumor" target="_self" style="color:#d1e9ff;font-size: 20px;">Brain Tumor</a> 
-        </li>
-        <li class="nav-item">
-         <a class="nav-link" href="https://saveyourlife.streamlit.app/About me" target="_self" style="color:#d1e9ff;font-size: 20px;">About me</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-"""
+
 st.markdown(menu(), unsafe_allow_html=True)
 
 def brand():
