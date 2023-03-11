5cef109996c54e24dcc560a69885348bdfb85454
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-11 16:38:05 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -129,4 +129,43 @@ def footer():
 </div>
 </div>
 """
+def footerasistente():
+  return """
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+<div class="footerasistente">
+  <div class="wrapper">
+      <div class="icon facebook">
+        <div class="tooltip">
+            Facebook
+        </div>
+        <span><i class="fab fa-facebook-f"></i></span>
+      </div>
+      <div class="icon twitter">
+        <div class="tooltip">
+            Twitter
+        </div>
+        <span><i class="fab fa-twitter"></i></span>
+      </div>
+      <div class="icon instagram">
+        <div class="tooltip">
+            Instagram
+        </div>
+        <span><i class="fab fa-instagram"></i></span>
+      </div>
+      <div class="icon github">
+        <div class="tooltip">
+            Github
+        </div>
+        <span><i class="fab fa-github"></i></span>
+      </div>
+      <div class="icon youtube">
+        <div class="tooltip">
+            YouTube
+        </div>
+        <span><i class="fab fa-youtube"></i></span>
+      </div>
+</div>
+</div>
+"""
 #
