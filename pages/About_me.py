b1284482b81c6f5b6e6fa27b3b61bd19e5065e29
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-12 01:05:24 +0100

    Update About_me.py

--- pages/About_me.py
+++ pages/About_me.py
@@ -2,10 +2,18 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import streamlit.components.v1 as components
+from st_pages import show_pages_from_config
 from functions import *
 
 #PageConfig
 page_config = importar_config()
+show_pages_from_config()
+#Menu
+menu = menu()
+#Footer
+footer = footer()
+
+st.markdown(menu, unsafe_allow_html=True)
 
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
@@ -13,5 +21,9 @@ chart_data = pd.DataFrame(
 
 st.bar_chart(chart_data)
 
-#CSS
+#footer       
+st.markdown(footer, unsafe_allow_html=True)
+#CCS
+importar_css('.streamlit/stylefooter.css') 
+importar_css('bootstrap/css/bootstrap.min.css')
 importar_css('.streamlit/style.css')
