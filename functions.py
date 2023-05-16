7cbb6aba31f6074ea76e0f63549f9db65cb06be5
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-05-16 18:44:49 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -92,11 +92,7 @@ def cards():
 #DONATE
 def donate():
   return """
-<form action="https://www.paypal.com/donate" method="post" target="_top">
-<input type="hidden" name="hosted_button_id" value="K9MYJDDJU35TN" />
-<input type="image" src="https://www.paypalobjects.com/es_ES/ES/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Botón Donar con PayPal" />
-<img alt="" border="0" src="https://www.paypal.com/es_ES/i/scr/pixel.gif" width="1" height="1" />
-</form>
+<<a href="https://www.paypal.com/donate?hosted_button_id=K9MYJDDJU35TN">''<img src="https://www.paypalobjects.com/es_ES/ES/i/btn/btn_donate_LG.gif" ''alt="Botón Donar con PayPal"></a>'
 """
 #FOOTER
 def footermain():
